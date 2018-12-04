d32e94cd4543d2964b6d59c4e6d09b4b7ab63d09
Matt Davis <matthewpeterdavis@gmail.com>
2018-12-04 22:47:38 +0200

    Update post.js

--- src/handlers/post.js
+++ src/handlers/post.js
@@ -1,27 +1,6 @@
 const qs = require('querystring')
 const queries = require('../database/queries.js')
 
-
-const register = (req, res) => {
-  var body = '';
-  req.on('data', (data) => {
-    body += data.toString();
-  });
-  req.on('end', () => {
-    const { email, password } = qs.parse(body)
-    // PART 1: Change the code below here!
-    // We should not be storing the password as plain text!
-
-    queries.addUserToDatabase(email, password, (err, result) => {
-      if (err) {
-        res.end('Error registering')
-        return
-      }
-      res.end('successfully registered!')
-    })
-  });
-}
-
 const login = (req, res) => {
   var body = '';
   req.on('data', (data) => {
@@ -41,6 +20,26 @@ const login = (req, res) => {
       res.end('Success Logging In!')
     })
   })
+}
+
+const register = (req, res) => {
+  var body = '';
+  req.on('data', (data) => {
+    body += data.toString();
+  });
+  req.on('end', () => {
+    const { email, password } = qs.parse(body)
+    // PART 1: Change the code below here!
+    // We should not be storing the password as plain text!
+
+    queries.addUserToDatabase(email, password, (err, result) => {
+      if (err) {
+        res.end('Error registering')
+        return
+      }
+      res.end('successfully registered!')
+    })
+  });
 }
 
 module.exports = {
