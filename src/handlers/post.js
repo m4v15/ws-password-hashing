778b2aa96b37c396f3c5f5a7f2ed850ff878bca0
Matt Davis <matthewpeterdavis@gmail.com>
2018-12-09 09:16:24 +0200

    make this branch work as a solution

--- src/handlers/post.js
+++ src/handlers/post.js
@@ -1,4 +1,6 @@
+const bcrypt = require('bcryptjs')
 const qs = require('querystring')
+
 const queries = require('../database/queries.js')
 
 const login = (req, res) => {
@@ -15,8 +17,31 @@ const login = (req, res) => {
         return
       }
       // PART 1 - check the passwords match before sending in logged in message!
-      res.statusCode = 200;
-      res.end('Success Logging In!')
+
+
+
+      if (!passwordInDatabase) {
+        res.statusCode = 403;
+        res.end('No details matching that user have been found')
+        return
+      }
+
+      bcrypt.compare(password, passwordInDatabase, (err, passwordsMatch) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end('Error logging in')
+          return
+        }
+
+        if (!passwordsMatch) {
+          res.statusCode = 403;
+          res.end('Incorrect username + password combination')
+          return
+        }
+
+        res.statusCode = 200;
+        res.end('Success Logging In!')
+      });
     })
   })
 }
@@ -32,14 +57,21 @@ const register = (req, res) => {
     // We should not be storing the password as plain text!
     // We will also now need to change the log in code above (that you changed in part 1) so that it can compare a hashed password...
 
-    queries.addUserToDatabase(email, password, (err, result) => {
-      if (err) {
+    bcrypt.hash(password, 8, (hashErr, hashedPassword) => {
+      if (hashErr) {
         res.statusCode = 500;
         res.end('Error registering')
         return
       }
-      res.statusCode = 200;
-      res.end('successfully registered!')
+      queries.addUserToDatabase(email, hashedPassword, (err, result) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end('Error registering')
+          return
+        }
+        res.statusCode = 200;
+        res.end('successfully registered!')
+      });
     })
   });
 }
